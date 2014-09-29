850b8ae7fc4e9dc03aaccf3412533ba9f7a2aea7
Philipp Rieber <p.rieber@gmail.com>
2014-09-29 12:40:49 +0200

    Update content/skills/title; improve asset dumping;

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,10 @@
-(function(d, s, id) {
-    var js, fjs = d.getElementsByTagName(s)[0];
-    if (d.getElementById(id)) return;
-    js = d.createElement(s); js.id = id;
-    js.src = "//connect.facebook.net/de_DE/all.js#xfbml=1";
-    fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));
+//(function(d, s, id) {
+//    var js, fjs = d.getElementsByTagName(s)[0];
+//    if (d.getElementById(id)) return;
+//    js = d.createElement(s); js.id = id;
+//    js.src = "//connect.facebook.net/de_DE/all.js#xfbml=1";
+//    fjs.parentNode.insertBefore(js, fjs);
+//}(document, 'script', 'facebook-jssdk'));
 
 $(function(){
     $('.with-tooltip').tooltip();
