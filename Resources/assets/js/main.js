f3059f9afd2c7643580cf84716bf7b8bfc6cd213
Philipp Rieber <p.rieber@gmail.com>
2015-07-08 20:20:13 +0200

    add Impressum/Privacy notes improve JS Google Analytics OptOut separate email template

--- Resources/assets/js/main.js
+++ Resources/assets/js/main.js
@@ -1,11 +1,23 @@
-//(function(d, s, id) {
-//    var js, fjs = d.getElementsByTagName(s)[0];
-//    if (d.getElementById(id)) return;
-//    js = d.createElement(s); js.id = id;
-//    js.src = "//connect.facebook.net/de_DE/all.js#xfbml=1";
-//    fjs.parentNode.insertBefore(js, fjs);
-//}(document, 'script', 'facebook-jssdk'));
-
 $(function(){
-    $('.with-tooltip').tooltip();
+    // $('.with-tooltip').tooltip();
 });
+
+// Google Analytics
+var gaProperty = 'UA-19245446-3';
+var disableStr = 'ga-disable-' + gaProperty;
+if (document.cookie.indexOf(disableStr + '=true') > -1) {
+    window[disableStr] = true;
+}
+function gaOptout(confirmationMessage) {
+    document.cookie = disableStr + '=true; expires=Thu, 31 Dec 2099 23:59:59 UTC; path=/';
+    window[disableStr] = true;
+    alert(confirmationMessage);
+}
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+ga('create', gaProperty, 'auto');
+ga('set', 'anonymizeIp', true);
+ga('send', 'pageview');
